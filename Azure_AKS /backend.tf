51c655f849f834b6a971aa6dc691a4978b8e538e
kunny_kobo <chibuike@kobo360.com>
2021-05-04 16:37:12 +0100

    update

---
+++ Azure_AKS /backend.tf
@@ -0,0 +1,10 @@
+#Creating a state in bucket for the aks tf
+
+terraform {
+  // backend "azurerm" {
+  //   resource_group_name  = "tf_state"
+  //   storage_account_name = "tfstate019"
+  //   container_name       = "tfstate"
+  //   key                  = "terraform.tfstate"
+  // }
+}
---
+++ Azure_AKS /outputs.tf
@@ -0,0 +1,26 @@
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "aks_node_rg" {
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+output "host" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_key
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
